9cfaf9b435c8fc430beddad9203eeee496d1eb1b
Emmanuel Briceno <emmanuel@still-water.com>
2015-12-18 04:38:19 -0800

    started header

--- index.php
+++ index.php
@@ -18,22 +18,36 @@
 
 
 <div class="page-bg">
-  
-  <header>header</header>
-  
-  <div class="page-content">
-    <aside>aside</aside>
-    
-    <section class="content">
-      <div>hero</div>
-      <article>article</article>
-      <article>article</article>
-      <article>article</article>
-      <article>article</article>
-    </section>
+
+  <div class="container-fluid">
+    <header class="row">
+      <h1 class="logo-stillwater col-md-2"><a href="url_here">Still Water</a></h1>
+      <nav class="primary col-xs-3 pull-right">
+        <ul>
+          <li><a href="url_here">item_1</a></li><!--
+          --><li><a href="url_here">item_2</a></li><!--
+          --><li><a href="url_here">item_3</a></li><!--
+          --><li><a href="url_here">item_4</a></li><!--
+          --><li><a href="url_here">item_5</a></li>
+        </ul>
+      </nav>
+    </header>
+  </div><!--/container-fluid 
+
+  <div class="page-content container-fluid">
+    <div class="row">
+      <aside class="col-md-2 fixed">aside</aside>
+      <section class="content col-md-10 scrollit">
+        <div>hero</div>
+        <article>article</article>
+        <article>article</article>
+        <article>article</article>
+        <article>article</article>
+      </section>
+    </div><!--/row -->
   </div><!--/page-content -->
-  
-  <footer>footer</footer>
+
+    <footer class="col-md-12">footer</footer>
 
 
 
@@ -46,23 +60,23 @@
 
 
 
-</div><!--/page-bg -->
+  </div><!--/page-bg -->
 
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
 
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-  function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-e.src='https://www.google-analytics.com/analytics.js';
-r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
+  <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+  <script>
+  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+  e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+  e.src='https://www.google-analytics.com/analytics.js';
+  r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+  ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+  </script>
 </body>
 </html>
